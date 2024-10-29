a84ac4f5e4dc8002ba90bffd70fde83b8086a906
Nathalie von Heyl <nath.vonheyl@gmail.com>
2024-10-29 15:04:12 +0000

    feat: add toggle down info box

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,22 @@ document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById("start-button");
     const startButtonContainer = document.getElementById("start-button-container");
     const questionCard = document.getElementById("question-card");
-    const timerContainer = document.getElementById("timer-container"); 
+    const timerContainer = document.getElementById("timer-container");
+    const infoToggle = document.getElementById("info-icon");
+    const infoBox = document.getElementById("info-box");
+    const quitButton = document.getElementById("quit-button");
+
+    // Collapsible info box 
+    
 
     // Hide Landing page layout and show Question session layout 
     function startSession() {
         // Hide landing page
         startButtonContainer.classList.add("hidden");
         // Show question session 
-        questionCard.classList.remove("hidden"); 
-        timerContainer.classList.remove("hidden"); 
+        questionCard.classList.remove("hidden");
+        timerContainer.classList.remove("hidden");
+        quitButton.classList.remove("hidden");
     }
 
     // Start the session 

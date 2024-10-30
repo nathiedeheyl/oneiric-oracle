b6e315b8aa5a8b75ea2ee1bd6b107ff7a46dd549
Nathalie von Heyl <nath.vonheyl@gmail.com>
2024-10-30 14:47:49 +0000

    feat: js f about when and how to update progress bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const startButton = document.getElementById("start-button");
     const startButtonContainer = document.getElementById("start-button-container");
-    const questionCard = document.getElementById("question-card-container");
+    const questionCard = document.getElementById("question-container");
     const timerContainer = document.getElementById("timer-container");
     const infoToggle = document.getElementById("info-icon");
     // const infoBox = document.getElementById("info-box");
@@ -43,3 +43,39 @@ document.addEventListener("DOMContentLoaded", function () {
     // Quit the session 
     quitButton.addEventListener("click", quitSession);
 })
+
+// Question value count and progress bar update 
+const totalQuestions = 7;
+let currentQuestionIndex = 0;
+
+// Insert - - Connection between answer buttons and f answerQuestions 
+document.addEventListener("DOMContentLoaded", function() {
+    let answerButtons = document.getElementsByClassName("answer-button");
+
+    for (let button of buttons) {}
+
+/**
+ * Update progress bar to track total amount of questions answered
+ */
+function updateProgressBar() {
+    const progressBar = document.getElementById("progress-bar");
+    // calculate percentage of progress 
+    let progressPercentage = ((currentQuestionIndex + 1 / totalQuestions) * 100)
+    // update width of progress bar 
+    progressBar.style.width = progressPercentage + "%";
+}
+
+/**
+ * Handle show next question, progress bar update or else 
+ * show feedback layout when all questions are answered
+ */
+function answerQuestion() {
+    currentQuestionIndex++;
+    if (currentQuestionIndex < totalQuestions) {
+        // Insert - - show next question?? 
+        // Show next question and update progress: 
+        updateProgressBar();
+    } else {
+        // Insert - - function to hide question session layout and display feedback layout 
+    }
+}

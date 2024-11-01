ddba3b8eb1c4f7f821579d186ad9bc146f669817
Nathalie von <nath.vonheyl@gmail.com>
2024-11-01 12:07:28 +0000

    add example questions to question catalogue

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,34 @@
 // Question catalogue 
 const questions = [
     {
-        
-    }];
+    question: 'In your dream, what kind of environmenet were you in?',
+        answers: [
+            { text: 'A peaceful, familiar place', score: 4 },
+            { text: 'A mysterious, unknown place', score: 3 },
+            { text: 'A place filled with activity and people', score: 2 },
+            { text: 'A calm, natural landscape', score: 1 },
+        ]
+}, 
+{
+    question: 'What were you doing?', 
+    answers: [
+        { text: 'Walking around exploring', score: 4}
+        { text: 'Flying or floating above the scene', score: 3}
+        { text: 'Running, possibly escaping from something', score: 2}
+        { text: 'Standing still, possibly ovserving the situation', score: 2}
+    ]
+}
 
-// Defined variables 
-    const startButton = document.getElementById("start-button");
-    const startButtonContainer = document.getElementById("start-button-container");
-    const questionCard = document.getElementById("question-container");
-    const timerContainer = document.getElementById("timer-container");
-    const infoToggle = document.getElementById("info-icon");
-    const quitButton = document.getElementById("quit-button");
+];
+
+
+// Definition of variables 
+const startButton = document.getElementById("start-button");
+const startButtonContainer = document.getElementById("start-button-container");
+const questionCard = document.getElementById("question-container");
+const timerContainer = document.getElementById("timer-container");
+const infoToggle = document.getElementById("info-icon");
+const quitButton = document.getElementById("quit-button");
 
 
 // Wait for the DOM to finish loading before allowing to start session 
@@ -38,23 +56,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const totalQuestions = 7;
 let currentQuestionIndex = 0;
 
-
-// Insert here - - function to store data-score and add to final sum to determine feedback 
-
-
 /**
- * Update progress bar to track total amount of questions answered
- */
-function updateProgressBar() {
-    const progressBar = document.getElementById("progress-bar");
-    // calculate percentage of progress 
-    let progressPercentage = ((currentQuestionIndex + 1 / totalQuestions) * 100);
-    // update width of progress bar 
-    progressBar.style.width = progressPercentage + "%";
-}
-
-/**
- * Show next question and answers on answer-buttons
+ * Show (next) question and answers on answer-buttons
  */
 function showQuestions() {
     let answerButtons = document.getElementsByClassName("answer-button");
@@ -65,10 +68,14 @@ function showQuestions() {
     }
 }
 
+// Click event for answer-buttons 
+
+
+// Insert here - - function to store data-score and add to final sum to determine feedback 
 
 
 /**
- * Handle show next question, progress bar update or else 
+ * Handle show next question, with a progress bar update or 
  * show feedback layout when all questions are answered
  */
 function answerQuestion() {
@@ -82,22 +89,34 @@ function answerQuestion() {
     }
 }
 
-// Insert clickevent here and call on answerQuestion 
+
+/**
+ * Function to calculate progress and update progress bar 
+ */
+function updateProgressBar() {
+    const progressBar = document.getElementById("progress-bar");
+    // calculate percentage of progress 
+    let progressPercentage = ((currentQuestionIndex + 1 / totalQuestions) * 100);
+    // update width of progress bar 
+    progressBar.style.width = progressPercentage + "%";
+}
 
 
+// Insert here - - function to hide question session layout and display feedback layout with corresponding feedback 
 
-    /**
-     * Hide Question session layout and return to Landing page layout
-     */
-    function quitSession() {
-        // Hide question session layout
-        questionCard.classList.add("hidden");
-        timerContainer.classList.add("hidden");
-        quitButton.classList.add("hidden");
-        // Show landing page layout 
-        startButtonContainer.classList.remove("hidden");
-        infoToggle.classList.remove("hidden");
-    }
 
-    // Quit the session 
-    quitButton.addEventListener("click", quitSession);
+/**
+ * Hide Question session layout and return to Landing page layout
+ */
+function quitSession() {
+    // Hide question session layout
+    questionCard.classList.add("hidden");
+    timerContainer.classList.add("hidden");
+    quitButton.classList.add("hidden");
+    // Show landing page layout 
+    startButtonContainer.classList.remove("hidden");
+    infoToggle.classList.remove("hidden");
+}
+
+// Quit the session 
+quitButton.addEventListener("click", quitSession);
